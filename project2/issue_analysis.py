778aba4ccafb91fa419b59d61091308b65005974
rnambis <rnambis@ncsu.edu>
2017-05-02 14:47:21 -0400

    Issue 6 done

--- project2/issue_analysis.py
+++ project2/issue_analysis.py
@@ -22,13 +22,70 @@ def percent_issues_user(conn,name):
 	plt.axis('equal')
 	plt.savefig(name+'_issues_per_user')
 	plt.show()
+
+def comments_per_user(conn, name):
+	cur = conn.cursor()
+	cur.execute('select distinct(user) from comment')
+	items = cur.fetchall()
+	users = [item.encode('utf-8') for i in items for item in i]
+	cur.execute('select distinct(issueID) from comment')
+	issues_total = cur.fetchall()
+	issues_total = [i for issue in issues_total for i in issue]
+	width = 0.35
+	cur.execute("select count(*), issueID from comment where user ='%s' group by issueID" %(users[0]))	
+	items = cur.fetchall()
+	y = []
+	issues = {}
+	for i in items:
+		issues[i[1]] = i[0]
+
+	for item in issues_total:
+		if item in issues:
+			y.append(issues[item])
+		else:
+			y.append(0)
+	#print items
+	color = ['blue','green','red','yellow','orange']
+	k = 0
+	j = -0.2
+	fig = plt.figure()
+	ax = plt.subplot(111)						
+	for user in users:
+		#print issues_total	
+		cur.execute("select count(*), issueID from comment where user ='%s' group by issueID" %(user))	
+		items = cur.fetchall()
+		y = []
+		issues = {}
+		for i in items:
+			issues[i[1]] = i[0]
+
+		for item in issues_total:
+			if item in issues:
+				y.append(issues[item])
+			else:
+				y.append(0)
+		#print issues_total
+		#print "hello"
+		temp = [float(x)+j for x in issues_total]		
+		ax.bar(temp, y, width = 0.2, color = color[k], align = 'center', label = str(user))
+		j+=0.2
+		#plt.plot(issues_total, y, color = color[k])
+		k+=1
+	ax.set_ylabel('No of comments')
+	ax.set_xlabel('Issue IDs')
+	ax.set_title('No of comments per issue per user')
+	ax.legend(loc='upper left')
+	plt.show()
 	
 def main():
 	group1 = sqlite3.connect('group1.db')
-	percent_issues_user(group1, 'group1')
+	#percent_issues_user(group1, 'group1')
+	
 	group2 = sqlite3.connect('group2.db')
-	percent_issues_user(group2,'group2')
+	#percent_issues_user(group2,'group2')
+
 	group3 = sqlite3.connect('group3.db')
-	percent_issues_user(group3,'group3')
+	#percent_issues_user(group3,'group3')
+	comments_per_user(group3,'group3')
 if __name__ == "__main__":
 	main()
